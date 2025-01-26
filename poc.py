8d84ee91039bee14b46ba04b7978640707188254
hectornunfer <hectornunfer@gmail.com>
2025-01-26 01:12:36 +0100

    Poc
    Poc

--- poc.py
+++ poc.py
@@ -1,59 +1,54 @@
 import os
 import re
 from pathlib import Path
-import torch
-from model import LogLLM
-from customDataset import replace_patterns
 import numpy as np
-
-# Configuración
-dataset_name = 'HDFS_v1'  # Asegúrate de que esto coincida con tu modelo entrenado
-Bert_path = r"google-bert/bert-base-uncased"
-Llama_path = r"meta-llama/Meta-Llama-3-8B"
+import torch
+from tqdm import tqdm
+from model import LogLLM
+from customDataset import CustomDataset
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 max_content_len = 100
 max_seq_len = 128
+dataset_name = 'HDFS_v1'   # 'Thunderbird' 'HDFS_v1'  'BGL'  'Liberty‘
+data_path = r'/content/{}/train.csv'.format(dataset_name)
+
+Bert_path = r"google-bert/bert-base-uncased"
+Llama_path = r"meta-llama/Meta-Llama-3-8B"
 
 ROOT_DIR = Path(__file__).parent
 ft_path = os.path.join(ROOT_DIR, r"ft_model_{}".format(dataset_name))
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0")
 
-def analyze_log(log_message, model):
-    """Analiza un mensaje de log usando el modelo LLM."""
-    processed_log = replace_patterns(log_message)
-    sequences = np.array([processed_log.split(' ;-; ')], dtype=object)
-    
+print(
+f'dataset_name: {dataset_name}\n'
+f'max_content_len: {max_content_len}\n'
+f'max_seq_len: {max_seq_len}\n'
+f'device: {device}')
+
+
+def evalModel(model, dataset):
     model.eval()
-    with torch.no_grad():
-        outputs_ids = model(sequences)
-        outputs = model.Llama_tokenizer.batch_decode(outputs_ids)
-        
-        for text in outputs:
-            matches = re.findall(r' (.*?)\.<|end_of_text|>', text)
-            if len(matches) > 0:
-                prediction = matches[0]
-            else:
-                prediction = ''
-            
-        if prediction == 'anomalous':
-            return "anomalous"
-        else:
-            return "normal"
+    pre = 0
 
+    preds = []
+
+    with torch.no_grad():
+        for i in range(len(dataset)):
+            this_batch_seq, _ = dataset.get_batch(i)
+            outputs_id = model(this_batch_seq)
+            output = model.Llama_tokenizer.batch_decode(outputs_id)
+            matches = re.findall(r' (.*?)\.<|end_of_text|>', output)
+            if len(matches) > 0:
+                preds.append(matches[0])
+                print(dataset.get_batch(i) + ': ' + matches[0])
+            else:
+                preds.append('')
 
 if __name__ == '__main__':
-    # Cargar el modelo
+    print(f'dataset: {data_path}')
+    dataset = CustomDataset(data_path)
     model = LogLLM(Bert_path, Llama_path, ft_path=ft_path, is_train_mode=False, device=device,
                    max_content_len=max_content_len, max_seq_len=max_seq_len)
-
-    # Log de ejemplo (reemplaza con tu log real)
-    log_message = "150610 14:45:20 12420 INFO dfs.DataNode:  Receiving block blk_-1608704935189617140 src: /10.250.18.18:50010 dest: /10.250.18.18:55786"
-    # log_message = "150610 14:45:20 12420 INFO dfs.DataNode:  Receiving block blk_-1608704935189617140 src: /10.250.18.18:50010 dest: /10.250.18.18:55786 ;-; 150610 14:45:20 12420 INFO dfs.DataNode:  Receiving block blk_-1608704935189617140 src: /10.250.18.18:50010 dest: /10.250.18.18:55786 ;-; 150610 14:45:20 12420 INFO dfs.DataNode:  Receiving block blk_-1608704935189617140 src: /10.250.18.18:50010 dest: /10.250.18.18:55786"
-
-    # Analizar el log
-    prediction = analyze_log(log_message, model)
-
-    # Imprimir el resultado
-    print(f"Log: {log_message}")
-    print(f"Predicción del modelo: {prediction}")
+    evalModel(model, dataset)
---
+++ prepareData/format.py
@@ -0,0 +1,85 @@
+import os.path
+
+
+import numpy as np
+import pandas as pd
+from helper import sliding_window, fixedSize_window, structure_log
+
+#### for Thunderbird, Liberty, BGL
+
+
+data_dir = r'/content/Thunderbird'
+log_name = "Thunderbird.log"
+
+start_line = 0
+end_line = None
+
+# # Liberty
+# start_line = 40000000
+# end_line = 45000000
+
+# # thunderbird
+# start_line = 160000000
+# end_line = 170000000
+
+output_dir = data_dir
+
+
+
+if __name__ == '__main__':
+    # group_type = 'time_sliding'
+
+    window_size = 100
+    step_size = 100
+
+    if 'thunderbird' in log_name.lower() or 'spirit' in log_name.lower() or 'liberty' in log_name.lower():
+        log_format = '<Label> <Id> <Date> <Admin> <Month> <Day> <Time> <AdminAddr> <Content>'   #thunderbird  , spirit, liberty
+    elif 'bgl' in log_name.lower():
+        log_format = '<Label> <Id> <Date> <Code1> <Time> <Code2> <Component1> <Component2> <Level> <Content>'  #bgl
+    else:
+        raise Exception('missing valid log format')
+    print(f'Auto log_format: {log_format}')
+
+    structure_log(data_dir, output_dir, log_name, log_format, start_line = start_line, end_line = end_line)
+
+    print(f'window_size: {window_size}; step_size: {step_size}')
+
+
+    df = pd.read_csv(os.path.join(output_dir,f'{log_name}_structured.csv'))
+
+    print(len(df))
+
+    df["Label"] = df["Label"].apply(lambda x: int(x != "-"))
+
+    df_aux = df.reset_index(drop=True)
+
+    print('Start grouping.')
+
+    session_df = fixedSize_window(
+        df_aux[['Content', 'Label']],
+        window_size=window_size, step_size=step_size
+    )
+
+    col = ['Content', 'Label','item_Label']
+    spliter=' ;-; '
+
+    session_df = session_df[col]
+    session_df['session_length'] = session_df["Content"].apply(len)
+    session_df["Content"] = session_df["Content"].apply(lambda x: spliter.join(x))
+
+    mean_session_len = session_df['session_length'].mean()
+    max_session_len = session_df['session_length'].max()
+    num_anomalous= session_df['Label'].sum()
+    num_normal = len(session_df['Label']) - session_df['Label'].sum()
+
+    session_df.to_csv(os.path.join(output_dir, 'formatted.csv'),index=False)
+
+    print('Dataset info:')
+    print(f"max session length: {max_session_len}; mean session length: {mean_session_len}\n")
+    print(f"number of anomalous sessions: {num_anomalous}; number of normal sessions: {num_normal}; number of total sessions: {len(session_df['Label'])}\n")
+
+    with open(os.path.join(output_dir, 'formatted_info.txt'), 'w') as file:
+        file.write(f"max session length: {max_session_len}; mean session length: {mean_session_len}\n")
+        file.write(f"number of anomalous sessions: {num_anomalous}; number of normal sessions: {num_normal}; number of total sessions: {len(session_df['Label'])}\n")
+
+
